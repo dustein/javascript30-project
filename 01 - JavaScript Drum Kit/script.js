e9d7ec4a4edb3e18ccd026dc33cf20dd0a1cdebb
dustein <steindu@gmail.com>
2022-07-04 13:38:13 -0300

    iniciando ex 25

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,19 +1,21 @@
-window.addEventListener('keydown', (e) => {
-   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-   //selecionar a div referente a cada tecla por meio da classe key
-   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-   if (!audio) return; //nao retorna null se for tecla sem audio associado
-   audio.currentTime = 0; ///"rebobina" o audio para permitir acionamento antes da trilha encerrar
-   audio.play()
-   key.classList.add('playing')
-})
-
 //para apagar a tecla apos ser pressionada...
 function removeTransition(e) {
    if (e.propretyName !== 'transform') return; //para nao pegar todos os eventos de transicao
 
-   this.classList.remove('playing')
+   e.target.classList.remove('playing')
 }
 
-const keys = document.querySelectorAll('.key');
+function playSound (e) {
+   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+   //selecionar a div referente a cada tecla por meio da classe key
+   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+   if (!audio) return; //nao retorna null se for tecla sem audio associado
+   key.classList.add('playing')
+   audio.currentTime = 0; ///"rebobina" o audio para permitir acionamento antes da trilha encerrar
+   audio.play()
+}
+
+
+const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
